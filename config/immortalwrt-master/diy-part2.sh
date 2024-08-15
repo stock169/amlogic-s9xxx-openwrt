79b131965eb30aae1d22280cbce98ac3df5b9385
stock169 <58556976+stock169@users.noreply.github.com>
2024-08-15 16:56:19 +0800

    Add files via upload

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -22,6 +22,46 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 
 # ------------------------------- Other started -------------------------------
 #
+wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/main/002-support-ape-mka-format.patch
+#rm -f feeds/packages/sound/mpd/Makefile
+#wget -P feeds/packages/sound/mpd/ https://raw.githubusercontent.com/stock169/amlogic-s9xxx-openwrt/main/Makefile
+sed -i 's/USER="mpd"/USER="root"/g' feeds/packages/sound/mpd/files/mpd.init
+sed -i 's/GROUP="mpd"/GROUP="root"/g' feeds/packages/sound/mpd/files/mpd.init
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/.git
+rm -rf feeds/luci/applications/luci-app-passwall/.github
+mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/net/pdnsd-alt
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+rm -rf feeds/packages/net/trojan-plus
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/xray-plugin
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/shadowsocks-rust
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/tuic-client
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/ssocks
+rm -rf feeds/packages/net/trojan-go
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/brook
+rm -rf feeds/packages/net/dns2tcp
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git feeds/packages/net/tmp
+rm -rf feeds/packages/net/tmp/.git
+rm -rf feeds/packages/net/tmp/.github
+rm -rf feeds/packages/net/tmp/gn
+mv feeds/packages/net/tmp/* feeds/packages/net
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
@@ -30,4 +70,10 @@ git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
-
+#sed -i 's/GO_PKG_BUILD_PKG:=$(GO_PKG)\/app/GO_PKG_BUILD_PKG:=$(GO_PKG)\/app\/v2/g' feeds/packages/net/hysteria/Makefile
+#sed -i 's/app\/cmd.appVersion/app\/v2\/cmd.appVersion/g' feeds/packages/net/hysteria/Makefile
+#sed -i 's/app\/cmd.appType/app\/v2\/cmd.appType/g' feeds/packages/net/hysteria/Makefile
+#sed -i 's/app\/cmd.appPlatform/app\/v2\/cmd.appPlatform/g' feeds/packages/net/hysteria/Makefile
+#sed -i 's/app\/cmd.appArch/app\/v2\/cmd.appArch/g' feeds/packages/net/hysteria/Makefile
+#./scripts/feeds update -a
+#./scripts/feeds install -a
