895b11760e18599c1a0907f3997baa960c80d11a
stock169 <58556976+stock169@users.noreply.github.com>
2024-12-28 15:04:56 +0800

    Add files via upload

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -11,4 +11,9 @@
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
-
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+#./scripts/feeds update istore
+#./scripts/feeds install -d y -p istore luci-app-store
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
