ef07e095ede2d4d614cf9377022293a0b450af34
stock169 <58556976+stock169@users.noreply.github.com>
2025-01-18 16:58:42 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -11,4 +11,12 @@
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
-
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+./scripts/feeds update openwrt-passwall-packages
+./scripts/feeds update openwrt-passwall
+./scripts/feeds install -d y -p openwrt-passwall luci-app-passwall
