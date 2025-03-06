6249501afab1dd9a68dd8993bdb30e0778e0e88d
stock169 <58556976+stock169@users.noreply.github.com>
2025-03-06 09:34:32 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -80,11 +80,7 @@ git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #./scripts/feeds update -a
 #./scripts/feeds install -a
 sudo apt install snap
-go version
-go list -m all
 sudo snap install go --classic
-go version
-go list -m all
 # go env -w GO111MODULE=on
 go mod tidy
 go version
