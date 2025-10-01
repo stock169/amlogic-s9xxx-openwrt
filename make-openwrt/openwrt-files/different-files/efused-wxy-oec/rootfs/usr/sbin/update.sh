e42fc934c6d275b5751e5c45d46601122be30d7a
stock169 <58556976+stock169@users.noreply.github.com>
2025-10-01 15:53:08 +0800

    Create update.sh

---
+++ make-openwrt/openwrt-files/different-files/efused-wxy-oec/rootfs/usr/sbin/update.sh
@@ -0,0 +1,17 @@
+cd /mnt/mmcblk0p7
+rm -rf !(extlinux)
+cd /
+cp -rn /boot/* /mnt/mmcblk0p7/
+umount /mnt/mmcblk0p8
+mount -t btrfs -o compress=zstd:9 /dev/mmcblk0p8  /mnt/mmcblk0p8
+cd /mnt/mmcblk0p8
+rm -rf bin lib sbin usr www 
+cd /mnt/mmcblk0p8/etc
+rm -rf !(config|fstab)
+cd /
+cp -rf bin /mnt/mmcblk0p8/
+cp -rf lib /mnt/mmcblk0p8/
+cp -rf sbin /mnt/mmcblk0p8/
+cp -rf usr /mnt/mmcblk0p8/
+cp -rf www /mnt/mmcblk0p8/
+cp -rn etc /mnt/mmcblk0p8/
