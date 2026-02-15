a04bc6f9488f797127105ed2a082fe86fd51f1c4
stock169 <58556976+stock169@users.noreply.github.com>
2026-02-15 11:06:58 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -27,8 +27,8 @@ wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubuserconten
 #wget -P feeds/packages/sound/mpd/ https://raw.githubusercontent.com/stock169/amlogic-s9xxx-openwrt/main/Makefile
 #rm -f feeds/packages/lang/rust/Makefile
 #wget -P feeds/packages/lang/rust/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/refs/heads/main/rust/Makefile
-#rm -f feeds/packages/lang/golang/golang/Makefile
-#wget -P feeds/packages/lang/golang/golang/ https://raw.githubusercontent.com/sbwml/packages_lang_golang/refs/heads/25.x/golang/Makefile
+rm -f feeds/packages/lang/golang/golang/Makefile
+wget -P feeds/packages/lang/golang/golang/ https://raw.githubusercontent.com/sbwml/packages_lang_golang/refs/heads/25.x/golang/Makefile
 rm -f feeds/packages/utils/podman/Makefile
 wget -P feeds/packages/utils/podman/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/refs/heads/main/Makefile
 rm -f feeds/packages/utils/docker/Makefile
